6714db4371d5ba5101c0504d854c756a5aaa6bec
Falk B. Schimweg <git@falk.schimweg.de>
2025-03-10 14:53:11 +0100

    enable multiprocessing

--- xifdr/boosting.py
+++ xifdr/boosting.py
@@ -3,11 +3,10 @@ from functools import partial
 import logging
 
 import numpy as np
-import pandas as pd
 from polars import col
 import polars as pl
-from scipy.optimize import differential_evolution, brute
-import psutil
+from scipy.optimize import  brute
+from multiprocessing import get_context
 from .fdr import full_fdr
 
 logger = logging.getLogger(__name__)
@@ -20,7 +19,8 @@ def boost(df: pl.DataFrame,
           ppi_fdr: (float, float) = (0.0, 1.0),
           boost_level: str = "ppi",
           boost_between: bool = True,
-          method: str = "brute"):
+          method: str = "brute",
+          n_jobs: int = 1):
     if method == 'brute':
         return boost_rec_brute(
             df=df,
@@ -30,7 +30,8 @@ def boost(df: pl.DataFrame,
             link_fdr=link_fdr,
             ppi_fdr=ppi_fdr,
             boost_level=boost_level,
-            boost_between=boost_between
+            boost_between=boost_between,
+            n_jobs=n_jobs
         )
     else:
         raise ValueError(f'Unkown boosting method: {method}')
@@ -44,7 +45,8 @@ def boost_rec_brute(df: pl.DataFrame,
                     boost_level: str = "ppi",
                     boost_between: bool = True,
                     countdown: int = 5,
-                    Ns: int = 3):
+                    Ns: int = 3,
+                    n_jobs: int = 1):
     start_params = (
         psm_fdr,
         pep_fdr,
@@ -69,39 +71,41 @@ def boost_rec_brute(df: pl.DataFrame,
     best_result = -1
     current_countdown = countdown
     current_best_params = None
-    while True:
-        # Find the best params for the current search space
-        best_params, result = brute(
-            func,
-            ranges=current_params,
-            Ns=Ns
-        )
-        # Make the result positive
-        result *= -1
-        # Check whether there was an improvement
-        if result <= best_result:
-            # If no improvement decrease countdown
-            current_countdown -= 1
-            if current_countdown == 0:
-                break
-        else:
-            print(f'New highscore: {result}')
-            # If improvement reset countdown and update best result/params
-            best_result = result
-            current_best_params = best_params
-            current_countdown = countdown
-        # Narrow search space
-        current_spreads /= 2
-        # Generate next params
-        next_params = ()
-        for i, p in enumerate(best_params):
-            # Clip params to initial values
-            next_params += ((
-                max(p - current_spreads[i]/2, start_params[i][0]),
-                min(p + current_spreads[i] / 2, start_params[i][1])
-            ),)
-        current_params = next_params
-        print(f'Next params: {next_params}')
+    with get_context("spawn").Pool(n_jobs) as pool:
+        while True:
+            # Find the best params for the current search space
+            best_params, result = brute(
+                func,
+                ranges=current_params,
+                Ns=Ns,
+                workers=pool.map
+            )
+            # Make the result positive
+            result *= -1
+            # Check whether there was an improvement
+            if result <= best_result:
+                # If no improvement decrease countdown
+                current_countdown -= 1
+                if current_countdown == 0:
+                    break
+            else:
+                print(f'New highscore: {result}')
+                # If improvement reset countdown and update best result/params
+                best_result = result
+                current_best_params = best_params
+                current_countdown = countdown
+            # Narrow search space
+            current_spreads /= 2
+            # Generate next params
+            next_params = ()
+            for i, p in enumerate(best_params):
+                # Clip params to initial values
+                next_params += ((
+                    max(p - current_spreads[i]/2, start_params[i][0]),
+                    min(p + current_spreads[i] / 2, start_params[i][1])
+                ),)
+            current_params = next_params
+            print(f'Next params: {next_params}')
     # Return the final best params
     return current_best_params
 
