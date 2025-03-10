9d4d55526d800eb10e42aaabee41e64128ac8ab9
Falk B. Schimweg <git@falk.schimweg.de>
2025-03-10 10:28:52 +0100

    Include link pos in swapping

--- xifdr/fdr.py
+++ xifdr/fdr.py
@@ -63,13 +63,15 @@ def full_fdr(df: pl.DataFrame | pd.DataFrame,
     # Swap peptides based on joined protein group
     swap_mask = df.select(
         (
-            col('sequence_p1')
-            +col('protein_p1').list.join(';')
-            +col('start_pos_p1').cast(pl.List(pl.String)).list.join(';')
+            col('sequence_p1')+'|'+
+            +col('protein_p1').list.join(';')+'|'+
+            +col('start_pos_p1').cast(pl.List(pl.String)).list.join(';')+'|'+
+            +col('link_pos_p1').cast(pl.List(pl.String)).list.join(';')
         ) > (
-            col('sequence_p2')
-            +col('protein_p2').list.join(';')
-            +col('start_pos_p2').cast(pl.List(pl.String)).list.join(';')
+            col('sequence_p2')+'|'+
+            +col('protein_p2').list.join(';')+'|'+
+            +col('start_pos_p2').cast(pl.List(pl.String)).list.join(';')+'|'+
+            +col('link_pos_p1').cast(pl.List(pl.String)).list.join(';')
         )
     ).to_series()
     pair_cols1 = ['decoy_p1', 'start_pos_p1', 'link_pos_p1', 'sequence_p1', 'protein_p1']
