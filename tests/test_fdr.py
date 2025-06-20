af899c79c4cf8384faccf81e5c3a8968f674f2e8
Falk B. Schimweg <git@falk.schimweg.de>
2025-06-20 09:36:50 +0200

    Add columns boosting on columns

--- tests/test_fdr.py
+++ tests/test_fdr.py
@@ -206,3 +206,19 @@ def test_boosting():
         n_jobs=3
     )
     print(fdrs)
+
+def test_column_boost():
+    samples = pl.read_parquet('tests/fixtures/sample_data.parquet')
+    samples = samples.with_columns(coverage=pl.col('coverage_p1')+pl.col('coverage_p2'))
+    cutoffs = boost(
+        samples,
+        csm_fdr=(0, 0.2),
+        link_fdr=(0.05, 0.05),
+        ppi_fdr=(0.05, 0.05),
+        boost_cols=['coverage'],
+        neg_boost_cols=['charge'],
+        points=3,
+        n_jobs=3
+    )
+    print(cutoffs)
+    pass
--- xifdr/boosting.py
+++ xifdr/boosting.py
@@ -17,6 +17,8 @@ def boost(df: pl.DataFrame,
           prot_fdr: (float, float) = (0.0, 1.0),
           link_fdr: (float, float) = (0.0, 1.0),
           ppi_fdr: (float, float) = (0.0, 1.0),
+          boost_cols: list = [],
+          neg_boost_cols: list = [],
           boost_level: str = "ppi",
           boost_between: bool = True,
           method: str = "manhattan",
@@ -77,6 +79,8 @@ def boost(df: pl.DataFrame,
             prot_fdr=prot_fdr,
             link_fdr=link_fdr,
             ppi_fdr=ppi_fdr,
+            boost_cols=boost_cols,
+            neg_boost_cols=neg_boost_cols,
             boost_level=boost_level,
             boost_between=boost_between,
             countdown=countdown,
@@ -105,6 +109,8 @@ def boost_manhattan(df: pl.DataFrame,
                     prot_fdr: (float, float) = (0.0, 1.0),
                     link_fdr: (float, float) = (0.0, 1.0),
                     ppi_fdr: (float, float) = (0.0, 1.0),
+                    boost_cols: list = [],
+                    neg_boost_cols: list = [],
                     boost_level: str = "ppi",
                     boost_between: bool = True,
                     countdown: int = 3,
@@ -118,12 +124,17 @@ def boost_manhattan(df: pl.DataFrame,
         link_fdr,
         ppi_fdr
     )
+    start_params += tuple((0.0, 1.0) for _ in boost_cols)
+    start_params += tuple((0.0, 1.0) for _ in neg_boost_cols)
+
     with closing(get_context('spawn').Pool(n_jobs)) as pool:
         best_params, result = manhattan(
             _optimization_template,
             kwargs=dict(
                 df=df,
-                boost_level = boost_level,
+                boost_cols=boost_cols,
+                neg_boost_cols=neg_boost_cols,
+                boost_level=boost_level,
                 boost_between=boost_between,
             ),
             ranges=start_params,
@@ -201,10 +212,32 @@ def boost_rec_brute(df: pl.DataFrame,
     return best_params
 
 
-def _optimization_template(fdrs,
+def _optimization_template(cutoffs,
                            df: pl.DataFrame,
+                           boost_cols: list = [],
+                           neg_boost_cols: list = [],
                            boost_level: str = "ppi",
                            boost_between: bool = True):
+    fdrs = cutoffs[:5]
+    col_levels = cutoffs[5:]
+    neg_col_levels = col_levels[len(boost_cols):]
+
+    for i, c in enumerate(boost_cols):
+        df = df.filter(
+            (
+                    (pl.col(c)-pl.col(c).min()) /
+                    (pl.col(c).max()-pl.col(c).min())
+            ) >= col_levels[i]
+        )
+
+    for i, c in enumerate(neg_boost_cols):
+        df = df.filter(
+            (
+                    (pl.col(c)-pl.col(c).min()) /
+                    (pl.col(c).max()-pl.col(c).min())
+            ) <= neg_col_levels[i]
+        )
+
     result = full_fdr(df, *fdrs, prepare_column=False)[boost_level]
     if boost_between:
         result = result.filter(col('fdr_group') == 'between')
@@ -214,6 +247,6 @@ def _optimization_template(fdrs,
     tp = tt - td + dd
     logger.debug(
         f'Estimated true positive matches: {tp}\n'
-        f'Parameters: {fdrs}'
+        f'Parameters: {cutoffs}'
     )
     return -tp
