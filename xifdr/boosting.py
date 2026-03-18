83093fdf26c71ed640fef2ea5a9fa3855125e338
Falk B. Schimweg <git@falk.schimweg.de>
2026-03-18 17:11:52 +0100

    introduce dummy context

--- xifdr/boosting.py
+++ xifdr/boosting.py
@@ -8,6 +8,7 @@ from polars import col
 import polars as pl
 from contextlib import closing
 from multiprocessing import get_context
+from multiprocessing.dummy import get_context as get_dummy_context
 from .fdr import full_fdr
 from .utils.column_preparation import prepare_columns
 from .utils.knee_finder import find_knees
@@ -160,6 +161,8 @@ def boost_manhattan(df: pl.DataFrame,
         n_jobs = max(max_mem_cpu, 1)
         n_jobs = min(n_jobs, os.cpu_count())
         logger.info(f"Using {n_jobs} CPUs based on available memory.")
+    if n_jobs == 1:
+        get_context = get_dummy_context
     with closing(get_context('spawn').Pool(n_jobs)) as pool:
         while True:
             grids = []
